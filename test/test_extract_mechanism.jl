9c6dbc912511d859f2cc9272f472a2e64fb56170
Hugo Chu <hugo.chu@manchester.ac.uk>
2024-02-23 15:25:05 +0000

    Unit test to test function: extract_mechanism

---
+++ test/test_extract_mechanism.jl
@@ -0,0 +1,8 @@
+################################
+# create a test for the extract_mechanism function
+################################
+@testset "Test extract_mechanism" begin
+    @testset "Test extract_mechanism with valid input" begin
+        @test extract_mechanism("/eqt_file/MCM_BCARY.eqn.txt") == 1601
+    end
+end
