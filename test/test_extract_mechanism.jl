9ce9e1c450e89f803bc45a20b3a886e9a5c86c3e
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2024-03-04 21:56:59 +0000

    change mechanism test to use relative path to input file

--- test/test_extract_mechanism.jl
+++ test/test_extract_mechanism.jl
@@ -3,6 +3,6 @@
 ################################
 @testset "Test extract_mechanism" begin
     @testset "Test extract_mechanism with valid input" begin
-        @test extract_mechanism("/eqt_file/MCM_BCARY.eqn.txt") == 1601
+        @test extract_mechanism("../eqt_file/MCM_BCARY.eqn.txt") == 1601
     end
 end
