a150c6781a1626cb829ea977cdcba7bfb4ecd161
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2024-03-04 21:46:45 +0000

    example command line argument interface

--- src/main.jl
+++ src/main.jl
@@ -4,12 +4,34 @@
 # Use the using keyword to import the exported functions from the module parse_equation.
 ########################################################################################
 
+# import command line argument parsing library
+using ArgParse
+
 # using the export function inside /src/parse_equation.jl
 include("parse_equation.jl")
 using .parse_equation
 
-# Call exported function: extract_mechanism from parse_equation module
-extract_mechanism("/eqt_file/MCM_BCARY.eqn.txt")
+function parse_commandline()
+    # Define the command line arguments
+    settings = ArgParseSettings()
+    @add_arg_table settings begin
+        "--eqt_file"
+        help="Path to the eqt file"
+        required=true
+        arg_type=String
+    end
+    return parse_args(settings)
+end
 
+function main()
+    parsed_args = parse_commandline()
+    println("eqt_file: ", parsed_args["eqt_file"])
+
+    extract_mechanism(parsed_args["eqt_file"])
+end
+# Call exported function: extract_mechanism from parse_equation module
+#extract_mechanism("/eqt_file/MCM_BCARY.eqn.txt")
+
+main()
 
 
